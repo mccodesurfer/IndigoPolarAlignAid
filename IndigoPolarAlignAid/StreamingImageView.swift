e627edec8e124b8a65d49ef4d4bc85b2e16c92a4
Greg Beaton <25520563+mccodesurfer@users.noreply.github.com>
2024-02-02 16:12:09 -0500

    Updated image filepath to the users filepath

--- IndigoPolarAlignAid/StreamingImageView.swift
+++ IndigoPolarAlignAid/StreamingImageView.swift
@@ -8,27 +8,23 @@
 import SwiftUI
 
 struct StreamingImageView: View {
-    
     private var filePath : URL?
     @StateObject private var monitor : FileMonitor
     @State var id = UUID()
     
-    
     init(){
-        filePath = URL(string: "file:/Users/greg/Library/Containers/GB.IndigoPolarAlignAid/Data/img_01.jpg")!
+        filePath = URL(string: "file:/Users/\(NSUserName())/Library/Containers/GB.IndigoPolarAlignAid/Data/img_01.jpg")!
         let monitor = FileMonitor(filePath: filePath!)
-        
         self._monitor = StateObject(wrappedValue: monitor)
         monitor.startMonitoring()
     }
     
-    
     var body: some View {
-        
         GeometryReader { geometry in
             AsyncImage(url: filePath) { image in
-                image.image?.resizable().aspectRatio(contentMode: .fit) .frame(maxWidth: geometry.size.width, maxHeight: geometry.size.height)
-                
+                image.image?
+                    .resizable().aspectRatio(contentMode: .fit)
+                    .frame(maxWidth: geometry.size.width, maxHeight: geometry.size.height)
             }.id(UUID())
             Text("count \(monitor.fileUpdateCount) \(filePath!.absoluteString)")
         }
